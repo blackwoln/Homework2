9105ca69fd6b3cd14db70170cfa8a70f041cbd84
vkray <vitalikkrainyuk@yandex.ru>
2023-04-03 15:12:46 +0300

    hm3: Задача 3 Немного не так понял задание, но потом дошло и все справил

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
-        task3();
     }
 
     public static void task1 () {
@@ -10,11 +9,23 @@ public class Main {
 
         var dog = 8.0;
         System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        dog = dog - 3.5;
+        System.out.println(dog);
 
         var cat = 3.6;
         System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+        cat = cat - 3.5;
+        System.out.println(cat);
 
         var paper = 763789;
+        System.out.println(paper);
+        paper = paper + 4;
+        System.out.println(paper);
+        paper = paper - 7636;
         System.out.println(paper);
 
     }
@@ -22,34 +33,7 @@ public class Main {
     public static void task2 () {
         System.out.println("Задача 2");
 
-        var dog = 8.0;
-        dog = dog + 4;
-        System.out.println(dog);
-
-        var cat = 3.6;
-        cat = cat + 4;
-        System.out.println(cat);
-
-        var paper = 763789;
-        paper = paper + 4;
-        System.out.println(paper);
 
     }
 
-    public static void task3 () {
-        System.out.println("Задача 3");
-
-        var dog = 8.0;
-        dog = dog - 3.5;
-        System.out.println(dog);
-
-        var cat = 3.6;
-        cat = cat - 1.6;
-        System.out.println(cat);
-
-        var paper = 763789;
-        paper = paper - 7639;
-        System.out.println(paper);
-
-    }
 }
