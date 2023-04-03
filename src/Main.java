9755c6e885f19455299ef3bf203c91cdda082edc
vkray <vitalikkrainyuk@yandex.ru>
2023-04-03 14:41:10 +0300

    hm2: Задача 2

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,13 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+
         var dog = 8.0;
         System.out.println(dog);
+
         var cat = 3.6;
         System.out.println(cat);
+
         var paper = 763789;
         System.out.println(paper);
 
@@ -17,6 +20,18 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
+
+        var dog = 8.0;
+        dog = dog + 4;
+        System.out.println(dog);
+
+        var cat = 3.6;
+        cat = cat + 4;
+        System.out.println(cat);
+
+        var paper = 763789;
+        paper = paper + 4;
+        System.out.println(paper);
 
     }
 }
